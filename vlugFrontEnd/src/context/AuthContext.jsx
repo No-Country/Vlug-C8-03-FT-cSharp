811e4e53b398704730d5a0535d1f874719c2a68b
marcio-git <marcio.dev.work@gmail.com>
2022-12-03 02:53:10 -0300

    context and hooks folders + AuthContext.jsx + useAuth.jsx created

---
+++ vlugFrontEnd/src/context/AuthContext.jsx
@@ -0,0 +1,67 @@
+import { createContext, useState, useEffect } from 'react'
+import { auth } from '../firebase'
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+
+export const AuthContext = createContext()
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState()
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        console.log(user.email + " is logged in!");
+      } else {
+        console.log('User is logged out!');
+      }
+      setCurrentUser(user)
+    })
+    return unsubscribe
+  }, [])
+
+  const signup = async (userName, email, password) => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password).catch((err) =>
+        console.log(err)
+      );
+      await updateProfile(auth.currentUser, { displayName: userName })
+        .catch(
+          (err) => console.log(err)
+        );
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const signin = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password)
+        .then((response) => {
+          // console.log(response)
+        })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const signout = async () => {
+    try {
+      return auth.signOut().then((response) => {
+        // console.log(response)
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const value = {
+    currentUser,
+    signup,
+    signin,
+    signout
+  }
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
